c764d0f3a0e706ff2ff58065085b70877ab7f007
Lysianthus <she@lysianth.us>
2018-08-24 20:24:36 +0800

    :fire: Remove configurations for 2038

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('dist'));
+app.use(express.static(__dirname + 'dist/'));
 app.get('/', function (request, response){
 	response.sendFile(__dirname + '/dist/index.html');
 });
 
-app.listen(parseInt(process.env._2038_PORT), e => {
-	if (e) {
-		console.error(e);
-		process.exit(1);
-	}	
-	console.log(process.env._2038_READY);	
-});
+app.listen(3000);
