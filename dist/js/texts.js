d95a7054ee81546d270ec941d5022a5738a51f2a
Lysianthus <she@lysianth.us>
2020-05-14 11:42:46 +0800

    :speech_bubble: Just a few things

--- dist/js/texts.js
+++ dist/js/texts.js
@@ -3,7 +3,7 @@ var texts = [
 		"title" : "About",
 		"content" : `
 			<p>♀ · 9w1 954 sp/so · true neutral · PH</p>
-			<p>Hi, I’m <i>Lysianthus</i>! I really like <i>milk</i>, <i>cats</i>, <i>whales</i>, and <i>websites</i>. 🎈 Aspiring materials engineer at 0700, pseudo-psychologist at 1400, hobbyist web developer + apprentice sysadmin at 2000, fangirl at 0000, and <i>learner forever</i>. 🐙</p>`
+			<p>Hi, I’m <i>Lysianthus</i>! I really like <i>milk</i>, <i>cats</i>, <i>whales</i>, and <i>websites</i>. 🎈 Aspiring materials engineer at 0700, pseudo-psychologist at 1400, hobbyist web developer and sysadmin at 2000, fangirl at 0000, and <i>learner forever</i>. 🐙</p>`
 	},
 	{
 		"title": "Projects",
@@ -21,7 +21,7 @@ var texts = [
 	{
 		"title": "Social",
 		"content" : `
-			<p>I <a target="_blank" href="https://twitter.com/lysnths">tweet</a>, occasionally <a target="_blank" href="https://whereisthenuance.tumblr.com">GIF</a>, am always <a target="_blank" href="https://www.goodreads.com/lysianthus">reading</a> <i>something</i>, watch <a target="_blank" href="https://letterboxd.com/Lysianthus/">movies</a> as an <i>escape</i>, also watch <a target="_blank" href="https://myanimelist.net/profile/driedfish">anime</a> as an <i>escape</i>, watch <a target="_blank" href="https://www.notion.so/lysianthus/6998e98e4b574600b8a001b9eaff95d0?v=de886b6d397f445a84e7ec63134062d3">series</a> as an <i>escape</i>, write <a target="_blank" href="https://archiveofourown.org/users/aretaic">fanfic</a> when the <i>mood</i> strikes, <a target="_blank" href="https://github.com/Lysianthus">code</a> for personal <i>entertainment</i>, and unfortunately lose at <a target="_blank" href="https://lichess.org/@/lysianthus">chess</a> a lot.</p>`
+			<p>I <a target="_blank" href="https://twitter.com/lysnths">tweet</a>, occasionally <a target="_blank" href="https://whereisthenuance.tumblr.com">GIF</a>, am always <a target="_blank" href="https://www.goodreads.com/lysianthus">reading</a> <i>something</i>, watch <a target="_blank" href="https://letterboxd.com/Lysianthus/">movies</a> as an <i>escape</i>, watch <a target="_blank" href="https://myanimelist.net/profile/driedfish">anime</a> as an <i>escape</i>, watch <a target="_blank" href="https://www.notion.so/lysianthus/6998e98e4b574600b8a001b9eaff95d0?v=de886b6d397f445a84e7ec63134062d3">series</a> as an <i>escape</i>, write <a target="_blank" href="https://archiveofourown.org/users/aretaic">fanfic</a> when the <i>mood</i> strikes, <a target="_blank" href="https://github.com/Lysianthus">code</a> for personal <i>entertainment</i>, and am trying to improve at <a target="_blank" href="https://lichess.org/@/lysianthus">chess</a>.</p>`
 	},
 	{
 		"title": "Contact",
