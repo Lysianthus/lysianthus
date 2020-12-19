62f8fde5bf56669e07daf0ce24cb5651600067a9
Lysianthus <she@lysianth.us>
2020-12-19 20:31:21 +0800

    :art: Organize the format of the code

--- dist/js/texts.js
+++ dist/js/texts.js
@@ -6,7 +6,7 @@ var texts = [
 			<p>Hi, I’m <i>Lysianthus</i>! I really like <i>milk</i>, <i>cats</i>, <i>whales</i>, and <i>websites</i>. 🎈 Aspiring materials engineer at 0700, pseudo-psychologist at 1400, hobbyist web developer and sysadmin at 2000, fangirl at 0000, and <i>learner forever</i>. 🐙</p>`
 	},
 	{
-		"title": "Projects",
+		"title" : "Projects",
 		"content" : `
 			<p>Some <i>personal projects</i> I have or am working on:</p>
 			<ul>
@@ -19,12 +19,12 @@ var texts = [
 			</ul>`
 	},
 	{
-		"title": "Social",
+		"title" : "Social",
 		"content" : `
 			<p>I <a target="_blank" href="https://twitter.com/lysnths">tweet</a>, occasionally <a target="_blank" href="https://whereisthenuance.tumblr.com">GIF</a>, am always <a target="_blank" href="https://www.goodreads.com/lysianthus">reading</a> <i>something</i>, watch <a target="_blank" href="https://letterboxd.com/Lysianthus/">movies</a> as an <i>escape</i>, watch <a target="_blank" href="https://anilist.co/user/driedfish/">anime</a> as an <i>escape</i>, watch <a target="_blank" href="https://www.notion.so/lysianthus/6998e98e4b574600b8a001b9eaff95d0?v=de886b6d397f445a84e7ec63134062d3">series</a> as an <i>escape</i>, write <a target="_blank" href="https://archiveofourown.org/users/aretaic">fanfic</a> when the <i>mood</i> strikes, <a target="_blank" href="https://github.com/Lysianthus">code</a> for personal <i>entertainment</i>, and am trying to improve at <a target="_blank" href="https://lichess.org/@/lysianthus">chess</a>.</p>`
 	},
 	{
-		"title": "Contact",
+		"title" : "Contact",
 		"content" : `
 			<p>You can say <i>hello</i> at <a target="_blank" href="mailto:she@lysianth.us">she@lysianth.us</a>. I’m also on Discord as Lysianthus#9473. I’m nice, I promise (<i>I think</i>). (◡‿◡✿)</p>`
 	}
