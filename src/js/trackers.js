11d210ca568a2b2e1e76323d51118dfeb9c10ab1
Lysianthus <she@lysianth.us>
2021-12-24 00:48:19 +0800

    :fire::sparkles: Remove Google Analytics code and add Matomo code

--- src/js/trackers.js
+++ src/js/trackers.js
@@ -1,13 +1,28 @@
+// statcounter
 var sc_project = 10274195;
 var sc_invisible = 1;
 var sc_security = "bc12d404";
 var scJsHost = (("https:" == document.location.protocol) ? "https://secure." : "http://www.");
 document.write("<sc" + "ript type='text/javascript' src='" + scJsHost + "statcounter.com/counter/counter.js'></" + "script>");
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+// google analytics
+/*(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-67527433-2', 'auto');
-ga('send', 'pageview');
+ga('send', 'pageview');*/
+
+// matomo
+var _paq = window._paq = window._paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+	var u="//tiramisu.bouvardia.blue/";
+	_paq.push(['setTrackerUrl', u+'matomo.php']);
+	_paq.push(['setSiteId', '3']);
+	var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+	g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+})();
