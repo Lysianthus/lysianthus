de4bcb06672eae746906fd0c18f7e0a357355b5c
Lysianthus <she@lysianth.us>
2022-07-15 19:27:35 +0800

    :fire: Remove social links to Mastodon and Tumblr

--- dist/js/texts.js
+++ dist/js/texts.js
@@ -3,7 +3,7 @@ var texts = [
 		"title" : "About",
 		"content" : `
 			<p>she/her · 9w1 954 sp/so · true neutral · PH</p>
-			<p>Hi, I’m <i>Lysianthus</i>! I really like <i>milk</i>, <i>cats</i>, <i>whales</i>, and <i>websites</i>. 🎈 Aspiring materials engineer at 0700, pseudo-psychologist at 1400, hobbyist web developer and sysadmin at 2000, fangirl at 0000, and <i>learner forever</i>. 🐙</p>`
+			<p>Hi, I’m <i>Lysianthus</i>! I really like <i>milk</i>, <i>cats</i>, <i>whales</i>, and <i>websites</i>. 🎈 Aspiring engineer at 0700, pseudo-psychologist at 1400, hobbyist web developer and sysadmin at 2000, fangirl at 0000, and <i>learner forever</i>. 🐙</p>`
 	},
 	{
 		"title" : "Domains",
@@ -22,7 +22,7 @@ var texts = [
 	{
 		"title" : "Social",
 		"content" : `
-			<p>I <a target="_blank" title="Twitter" href="https://twitter.com/lysianthus">tweet</a>/<a rel="me" target="_blank" title="Mastodon.social" href="https://mastodon.social/@lysianthus">toot</a>, occasionally <a target="_blank" title="Tumblr" href="https://whereisthenuance.tumblr.com">GIF</a>, am always <a target="_blank" title="Goodreads" href="https://www.goodreads.com/lysianthus">reading</a> <i>something</i>, watch <a target="_blank" title="Letterboxd" href="https://letterboxd.com/Lysianthus/">movies</a> as an <i>escape</i>, watch <a target="_blank" title="AniList" href="https://anilist.co/user/driedfish/">anime</a> as an <i>escape</i>, watch <a target="_blank" href="https://www.notion.so/lysianthus/6998e98e4b574600b8a001b9eaff95d0?v=de886b6d397f445a84e7ec63134062d3">series</a> as an <i>escape</i>, write <a target="_blank" title="Archive of Our Own" href="https://archiveofourown.org/users/aretaic">fanfic</a> when the <i>mood</i> strikes, <a target="_blank" title="Github" href="https://github.com/Lysianthus">code</a> for personal <i>entertainment</i>, and am trying to improve at <a target="_blank" title="Lichess" href="https://lichess.org/@/lysianthus">chess</a>.</p>`
+			<p>I <a target="_blank" title="Twitter" href="https://twitter.com/lysianthus">tweet</a>, am always <a target="_blank" title="Goodreads" href="https://www.goodreads.com/lysianthus">reading</a> <i>something</i>, watch <a target="_blank" title="Letterboxd" href="https://letterboxd.com/Lysianthus/">movies</a> as an <i>escape</i>, watch <a target="_blank" title="AniList" href="https://anilist.co/user/driedfish/">anime</a> as an <i>escape</i>, watch <a target="_blank" href="https://www.notion.so/lysianthus/6998e98e4b574600b8a001b9eaff95d0?v=de886b6d397f445a84e7ec63134062d3">series</a> as an <i>escape</i>, write <a target="_blank" title="Archive of Our Own" href="https://archiveofourown.org/users/aretaic">fanfic</a> when the <i>mood</i> strikes, <a target="_blank" title="Github" href="https://github.com/Lysianthus">code</a> for personal <i>entertainment</i>, and am trying to improve at <a target="_blank" title="Lichess" href="https://lichess.org/@/lysianthus">chess</a>.</p>`
 	},
 	{
 		"title" : "Contact",
